9d34155b9a9c791933d76d2f2b00061d4a5edd18
josephpv <josephpv@google.com>
2025-03-10 14:51:37 +0000

    Change to not show face icon for history suggestions
    Screenshot:
    Before: go/ss/8EeYtQkanNPDEWL.png
    After: go/ss/4pqpkWfEzGmXpE4.png
    
    Bug: 401184113
    Test: Manual
    Flag: EXEMPT Bug fix
    Change-Id: Ia21c09daf08920f746843570d9bd8076ecf48c84

--- photopicker/src/com/android/photopicker/features/search/Search.kt
+++ photopicker/src/com/android/photopicker/features/search/Search.kt
@@ -789,7 +789,8 @@ fun SuggestionItem(suggestion: SearchSuggestion) {
         }
         val text = suggestion.displayText ?: ""
         Text(text = text, modifier = Modifier.padding(start = MEASUREMENT_LARGE_PADDING).weight(1f))
-        if (suggestion.type != SearchSuggestionType.FACE && suggestion.icon != null) {
+        if (suggestion.type != SearchSuggestionType.FACE
+                && suggestion.type != SearchSuggestionType.HISTORY && suggestion.icon != null) {
             ShowSuggestionIcon(suggestion, Modifier.size(MEASUREMENT_OTHER_ICON).clip(CircleShape))
         }
     }
